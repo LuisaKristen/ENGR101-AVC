2159d6f1ef57e69a14b8b4badb368b1b46622db7
lefrancais <seanreilly123@hotmail.com>
2018-05-07 12:17:41 +1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "E101.h"
 #include <stdio.h>
 
-init();
+int forward(int speed){
+  set_motor(1,speed);
+  set_motor(2,speed);
+}
+int stop(){
+  set_motor(1,0);
+  set_motor(2,0);
+}
 
-printf("Heyo Nathan Wride wassssup!");
+int main(){
+  init();
+  foward(50);
+  sleep1(3,0);
+  stop();
 
-return 0;
+  return 0;
 }
