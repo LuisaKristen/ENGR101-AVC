53946a41c92af81006b52741b181c1678fb15020
champ112 <38932403+champ112@users.noreply.github.com>
2018-05-08 11:54:20 +1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,6 +29,78 @@ int hard_turn_right(int turn){ //hard turning for Q2 part where it is all sharp 
   sleep1(0,turn);
   forward();
 }
+#include <stdio.h>
+#include "E101.h"
+
+int main ()
+{
+	init();
+}
+
+void openGate() {
+	char[] ipAddr;
+	strcopy(ipAddr, "192.168.1.1".c_str()); 	//Convert string to character array (easier than declaring a character array with the IP address).
+	
+	char[] message;
+	strcopy(message, "please".c_string());
+	
+	connect_to_server(ipAddr, 1000); 	//Establish a connection to the server
+	send_to_server(message); 	//Request the password from the server
+	
+	char[24] response;
+	recieve_from_server(response);	 //Store the server response
+	
+	send_to_server(response); 	//Transmit the response back to the server
+	
+	//Gate is now open.
+  
+}
+
+int whiteThreshold;
+
+void updateWhiteThreshold() {
+	int min = 255;
+	int max = 0;
+	
+	take_picture();
+	
+	for (int r = 0; r < 240; r++) { //#of rows
+		for (int c = 0; c < 320; c++) { //# of columns
+			int pixel = get_pixel(r, c, 3);
+			
+			if (pixel > max) {
+				max = pixel;
+			}
+			else if (pixel < min) {
+				min = pixel;
+			}
+		}
+	}
+	
+	//whiteThreshold is a global variable.
+	whiteThreshold = (max - min) / 2;
+}
+
+int getErrorSignal() {
+     updateWhiteThreshold();
+	take_picture();
+	
+	int error = 0;
+	int whitePixelCount = 0;
+	
+	for (int i = 0; i < 320; i++) {
+		int pixel = get_pixel(120, i, 3);
+		
+		error += (i - 160) * pixel;
+		
+		if (pixel > whiteThreshold) {
+			whitePixelCount++;
+		}
+	}
+	
+	return error / whitePixelCount;
+}
+
 
 void gate1() {
 	char password[24];
